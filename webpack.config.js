09beb7da1ecb15d4afe3302c20e0bb6438c32cef
HShaw215 <hu.shaw215@gmail.com>
2023-08-29 13:52:56 -0400

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules\/(?!react-router-dom)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            sourceMap: false,
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          },
+        ],
+      },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'client/components/public'),
+      publicPath: 'bundle.js',
+    },
+    proxy: {
+      '/api/**': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+}
