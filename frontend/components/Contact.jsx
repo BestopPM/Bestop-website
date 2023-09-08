88496e33a10133e18f4ce5d0a26d0b42dcf2e573
HShaw215 <hu.shaw215@gmail.com>
2023-09-08 19:16:33 -0400

    configured outline of contact page

---
+++ frontend/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../stylesheet/styles.css';
+
+const Contact = () => {
+    return (
+        <>
+        <p>Send Us A Message</p>
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <p>Mailing Address:</p>
+                <p>47000 Warm Springs Blvd Ste 274 Fremont, CA 94539</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Contact
