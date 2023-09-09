4ad2ed6583aec280ef74147c9ab31c46a1ec888e
HShaw215 <hu.shaw215@gmail.com>
2023-09-09 16:05:44 -0400

    Configured layout for message input

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -4,11 +4,20 @@ import '../stylesheet/styles.css';
 const Contact = () => {
     return (
         <>
+        {/* <Nav /> */}
         <p>Send Us A Message</p>
         <div>
-            <div>
-
-            </div>
+            <form>
+                <p>Name</p>
+                <input></input>
+                <p>Phone</p>
+                <input></input>
+                <p>Email</p>
+                <input></input>
+                <p>Message</p>
+                <input></input>
+                <button type='submit'>Submit</button>
+            </form>
             <div>
                 <p>Mailing Address:</p>
                 <p>47000 Warm Springs Blvd Ste 274 Fremont, CA 94539</p>
