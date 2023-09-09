edc3f821f9bc5a7683580c7018527ea329ab7ee9
HShaw215 <hu.shaw215@gmail.com>
2023-09-09 16:09:00 -0400

    completed inital contact page layout

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -21,6 +21,13 @@ const Contact = () => {
             <div>
                 <p>Mailing Address:</p>
                 <p>47000 Warm Springs Blvd Ste 274 Fremont, CA 94539</p>
+                <p>Email:</p>
+                <p>Phone:</p>
+                <span>(510)936-8686</span>
+                <p>Business Hours:</p>
+                <p>Mon-Fri   9:00am - 6:00pm</p>
+                <p>Sat-Sun   Closed</p>
+                <a href='/tenant'>Maintenance Request</a>
             </div>
         </div>
         </>
