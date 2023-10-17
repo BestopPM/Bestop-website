46f7bb8d12159a52599f987450a983625171fb0a
pearlhchang <p.chang1802@gmail.com>
2023-10-16 18:49:46 -0700

    add Navbar

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return(
+    <nav>
+      <div>
+        <a href="/" className="">
+          About Us
+        </a>
+        <a href="/" className="">
+          Owner Services
+        </a>
+        <a href="/" className="">
+          Tenant Services
+        </a>
+        <a href="/" className="">
+          Available Rentals
+        </a>
+        <a href="/" className="">
+          Contact Us
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
