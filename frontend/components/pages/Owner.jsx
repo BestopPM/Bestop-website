9257459be6fc90abb590461bad268b0ebeb53e40
pearlhchang <p.chang1802@gmail.com>
2023-10-18 23:28:42 -0700

    add owner services content

--- frontend/components/pages/Owner.jsx
+++ frontend/components/pages/Owner.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Owner = () => {
-  return(
+  return (
     <>
-    <h1>Owner Services</h1>
+      <header>
+        <h1>Owner Services</h1>
+      </header>
+
+      <main>
+        <h2>Overview</h2>
+        <p>
+          BESTOP PROPERTY MANAGEMENT takes a customized, individual approach to
+          working with property owners. We conduct regular visits, ensuring we
+          are proactive about maintenance and working with fairly priced,
+          quality vendors to preserve and protect owner’s investments.
+        </p>
+
+        <section>
+          <p>PROPERTY MANAGEMENT including:</p>
+          <ul>
+            <li>
+              Initial inspection & coordination of work required to get property
+              “Rent Ready”
+            </li>
+            <li>Determine rental price based on market analysis</li>
+            <li>Advertising</li>
+            <li>Showing</li>
+            <li>Tenant screening and Selection</li>
+            <li>Move-in Inspection of the property</li>
+            <li>Execution of Rental/Lease Agreement</li>
+            <li>Rent Collection</li>
+            <li>Property Repair and Maintenance</li>
+            <li>
+              Service Legal Notices, Process Evictions and Collections as
+              Required
+            </li>
+            <li>Provide Monthly and Yearly Financial Statement</li>
+          </ul>
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
 export default Owner;
