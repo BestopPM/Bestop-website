5ae7e8d216c7103e1d575b113673a118402473b3
HShaw215 <hu.shaw215@gmail.com>
2023-11-08 14:10:50 -0500

    added placeholder image

--- frontend/components/pages/Rentals.jsx
+++ frontend/components/pages/Rentals.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../../stylesheet/styles.css';
 
 const Rentals = () => {
   return(
     <>
-    <h1>Available Rentals</h1>
-    <div>
-      <div>
-
-      </div>
-      <div>
+    <h1 id ='message'>Available Rentals</h1>
+    <div id='contact'>
+      <div id='box1'>
+        <div id='rentalImage'>
+          <img src='https://i.imgur.com/HXS98BO_d.webp?maxwidth=760&fidelity=grand'></img>
+        </div>
         <p>Rental Price</p>
         <p>Property Adress</p>
         <a>Additional Details</a>
