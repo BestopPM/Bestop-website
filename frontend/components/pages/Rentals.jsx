c9b77dc1274fafc90a4e40dbc32d946ad059b0f6
HShaw215 <hu.shaw215@gmail.com>
2023-11-08 14:23:36 -0500

    configured outline for rental application

--- frontend/components/pages/Rentals.jsx
+++ frontend/components/pages/Rentals.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import '../../stylesheet/styles.css';
 
 const Rentals = () => {
+
+  const sendForm = (e) => {
+      e.preventDefault();
+
+      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text)
+          })
+  }
+
   return(
     <>
     <h1 id ='rentalMessage'>Available Rentals</h1>
@@ -11,14 +23,14 @@ const Rentals = () => {
           <img src='https://i.imgur.com/HXS98BO_d.webp?maxwidth=760&fidelity=grand'></img>
         </div>
         <div id='rentalInfo'>
-          <p>Rental Price</p>
-          <p>Property Address</p>
-          <a>Additional Details</a>
+          <p>Rental Price: </p>
+          <p>Property Address: </p>
+          <a>Additional Details: </a>
           <button id='showingButton'>Request a Showing</button>
         </div>
       </div>
     </div>
-    <a id='rentalApp'>Rental Application</a>
+    <a id='rentalApp' href='/rentalApp'>Rental Application</a>
     </>
   )
 }
