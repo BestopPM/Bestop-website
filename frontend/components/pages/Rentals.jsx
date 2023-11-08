64ad803e6a8c07f3709f686eef64fda80c70cfd7
HShaw215 <hu.shaw215@gmail.com>
2023-11-08 14:07:15 -0500

    outlined rental page format

--- frontend/components/pages/Rentals.jsx
+++ frontend/components/pages/Rentals.jsx
@@ -4,6 +4,18 @@ const Rentals = () => {
   return(
     <>
     <h1>Available Rentals</h1>
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <p>Rental Price</p>
+        <p>Property Adress</p>
+        <a>Additional Details</a>
+        <button>Request a Showing</button>
+      </div>
+      <a>Rental Application</a>
+    </div>
     </>
   )
 }
