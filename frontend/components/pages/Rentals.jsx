dca746874ddfe3d306d7eb4ae6990ee9f466470e
HShaw215 <hu.shaw215@gmail.com>
2023-11-08 14:18:40 -0500

    outlined style for rentals page

--- frontend/components/pages/Rentals.jsx
+++ frontend/components/pages/Rentals.jsx
@@ -4,19 +4,21 @@ import '../../stylesheet/styles.css';
 const Rentals = () => {
   return(
     <>
-    <h1 id ='message'>Available Rentals</h1>
+    <h1 id ='rentalMessage'>Available Rentals</h1>
     <div id='contact'>
-      <div id='box1'>
+      <div id='rental'>
         <div id='rentalImage'>
           <img src='https://i.imgur.com/HXS98BO_d.webp?maxwidth=760&fidelity=grand'></img>
         </div>
-        <p>Rental Price</p>
-        <p>Property Adress</p>
-        <a>Additional Details</a>
-        <button>Request a Showing</button>
+        <div id='rentalInfo'>
+          <p>Rental Price</p>
+          <p>Property Address</p>
+          <a>Additional Details</a>
+          <button id='showingButton'>Request a Showing</button>
+        </div>
       </div>
-      <a>Rental Application</a>
     </div>
+    <a id='rentalApp'>Rental Application</a>
     </>
   )
 }
