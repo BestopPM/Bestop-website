391b41616cce933ef2b6860b610bcff570afbf74
pearlhchang <p.chang1802@gmail.com>
2023-11-19 19:10:01 -0800

    remove MaintenanceModal.jsx from files

--- frontend/components/pages/Tenant.jsx
+++ frontend/components/pages/Tenant.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { useState } from "react";
-import MaintenanceModal from "../MaintenanceModal";
 import { Link } from "react-router-dom";
 
 const Tenant = () => {
-  const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
       <main>
         <section className="Tenant Services">
           <h1>Tenant Services</h1>
-          <button
-            classNmae="openModalBtn"
-            onClick={() => {
-              setOpenModal(true);
-            }}
-          >
+          <button>
             Maintenance Request
           </button>
-          {openModal && <MaintenanceModal closeModal={setOpenModal} />}
           <p>
             Please describe the problem in as much detail as possible. Attach
             photos or video can help property manager hire the right vendor for
