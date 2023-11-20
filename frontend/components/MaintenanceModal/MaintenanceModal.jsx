74b8c95835609465e17ca219b579ec2f6beab716
pearlhchang <p.chang1802@gmail.com>
2023-11-20 15:02:59 -0800

    add name, email, phone, address, message fields in MaintenanceModal

--- frontend/components/MaintenanceModal/MaintenanceModal.jsx
+++ frontend/components/MaintenanceModal/MaintenanceModal.jsx
@@ -38,7 +38,82 @@ const MaintenanceModal = ({ onSumbit, isOpen, onClose }) => {
   
   return (
     <Modal hasCloseBtn={true} isOpen={isOpen} onClose={onClose}>
-
+      <form onSubmit={handleSubmit}>
+        {/* name */}
+        <div className='form-row'>
+          <input
+            ref={focusInputRef}
+            type="text"
+            id="name"
+            name="name"
+            maxLength="50"
+            placeholder="Name*"
+            value={formState.name}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {/* phone */}
+        <div className='form-row'>
+          <input
+            ref={focusInputRef}
+            type="tel"
+            id="phone"
+            name="phone"
+            pattern="\(?\d{3}\)?-? *\d{3}-? *-?\d{4}"
+            placeholder="Phone Number*"
+            value={formState.phone}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <input
+            ref={focusInputRef}
+            type="email"
+            id="email"
+            name="email"
+            maxLength="50"
+            placeholder="Email Address*"
+            value={formState.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {/* address */}
+        <div className='form-row'>
+          <input
+            ref={focusInputRef}
+            type="text"
+            id="address"
+            name="address"
+            maxLength="50"
+            placeholder="Home Address*"
+            value={formState.address}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        {/* message */}
+        <div className='form-row'>
+          <textarea
+            ref={focusInputRef}
+            type="text"
+            id="message"
+            name="message"
+            maxLength="600"
+            placeholder="Please describe the issue in as much detail as possible*"
+            value={formState.message}
+            onChange={handleInputChange}
+            required
+          ></textarea>
+        </div>
+        {/* submit button */}
+        <div className="form-row">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </Modal>
   )
 }
