ea388f6177e3c2cf086e613811e18243f2401ba5
pearlhchang <p.chang1802@gmail.com>
2023-11-27 20:08:24 -0800

    update ShowingModal and MaintenanceModal

--- frontend/components/ShowingModal/ShowingModal.jsx
+++ frontend/components/ShowingModal/ShowingModal.jsx
@@ -3,7 +3,12 @@ import Modal from "../modal/modal";
 import emailjs from "emailjs-com";
 import "../../stylesheet/styles.css";
 
-const initialShowingModalData = {};
+const initialShowingModalData = {
+  name: "",
+  phone: "",
+  email: "",
+  message: "",
+};
 
 const ShowingModal = ({ onSubmit, isOpen, onClose }) => {
   const focusInputRef = useRef(null);
@@ -56,50 +61,54 @@ const ShowingModal = ({ onSubmit, isOpen, onClose }) => {
   return (
     <Modal hasCloseBtn={true} isOpen={isOpen} onClose={onClose}>
       <form ref={form} onSubmit={sendEmail}>
-      <div class='showing'>
-        <input placeholder="Name"
-              ref={focusInputRef}
-              type="text"
-              id="name"
-              name="name"
-              maxLength="50"
-              value={formState.name}
-              onChange={handleInputChange}
-              required
+        <div className="showing">
+          <h3> Request a Showing </h3>
+          <input
+            placeholder="Name*"
+            ref={focusInputRef}
+            type="text"
+            id="name"
+            name="name"
+            maxLength="50"
+            value={formState.name}
+            onChange={handleInputChange}
+            required
           ></input>
-        <input placeholder="Phone Number"
-          type="tel"
-          id="phone"
-          name="phone"
-          pattern="\(?\d{3}\)?-? *\d{3}-? *-?\d{4}"
-          value={formState.phone}
-          onChange={handleInputChange}
-          required
-        ></input>
-        <input placeholder="Email"
-          type="email"
-          id="email"
-          name="email"
-          maxLength="50"
-          value={formState.email}
-          onChange={handleInputChange}
-          required
-        ></input>
-        <textarea placeholder="Please list convenient times to view property"
-          ref={focusInputRef}
-          type="text"
-          id="textArea"
-          name="name"
-          maxLength="300"
-          value={formState.name}
-          onChange={handleInputChange}
-          required
-        ></textarea>
-      </div>
-     
-      <button type="submit" id="contactButton">
-            Submit
-          </button>
+          <input
+            placeholder="Phone Number*"
+            type="tel"
+            id="phone"
+            name="phone"
+            pattern="\(?\d{3}\)?-? *\d{3}-? *-?\d{4}"
+            value={formState.phone}
+            onChange={handleInputChange}
+            required
+          ></input>
+          <input
+            placeholder="Email*"
+            type="email"
+            id="email"
+            name="email"
+            maxLength="50"
+            value={formState.email}
+            onChange={handleInputChange}
+            required
+          ></input>
+          <textarea
+            placeholder="Please list convenient times to view the property*"
+            type="text"
+            id="textArea"
+            name="message"
+            maxLength="500"
+            value={formState.message}
+            onChange={handleInputChange}
+            required
+          ></textarea>
+        </div>
+
+        <button type="submit" id="contactButton">
+          Submit
+        </button>
       </form>
     </Modal>
   );
