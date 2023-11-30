2bf1c6b055f4676dfcbbc76b09ce5d68a151a7b2
HShaw215 <hu.shaw215@gmail.com>
2023-11-30 11:52:44 -0500

    cleaned up code

--- frontend/components/pages/Submit.jsx
+++ frontend/components/pages/Submit.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../../stylesheet/styles.css";
+
 const Submit = () => {
     return (
         <>
         <h1>Your request has been recieved. A property manager should be in contact within 24 hours.</h1>
         </>
     )
-};
+}
 
 export default Submit
