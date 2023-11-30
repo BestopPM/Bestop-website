b9ea366a3e633284aaefe232b680dca77fc6f1ae
pearlhchang <p.chang1802@gmail.com>
2023-11-30 13:42:57 -0800

    add styling for navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return(
-    <nav>
-      <h1>Logo</h1>
-      <div>
-        <Link to="/about">
-          About Us
-        </Link>
-        <Link to="/owner">
-          Owner Services
-        </Link>
-        <Link to="/tenant">
-          Tenant Services
-        </Link>
-        <Link to="/rentals">
-          Available Rentals
-        </Link>
-        <Link to="/contact">
-          Contact Us
-        </Link>
-      </div>
-    </nav>
-  )
-}
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to="/" className="navbar__item">
+            Logo
+          </Link>
+          <Link to="/about" className="navbar__item">
+            About Us
+          </Link>
+          <Link to="/owner" className="navbar__item">
+            Owner Services
+          </Link>
+          <Link to="/tenant" className="navbar__item">
+            Tenant Services
+          </Link>
+          <Link to="/rentals" className="navbar__item">
+            Available Rentals
+          </Link>
+          <Link to="/contact" className="navbar__item">
+            Contact Us
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
