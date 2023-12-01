c694e5529eb638771cfa38c22e54664fc48d1630
pearlhchang <p.chang1802@gmail.com>
2023-11-30 21:37:07 -0800

    update import statement

--- frontend/components/ShowingModal/ShowingModal.jsx
+++ frontend/components/ShowingModal/ShowingModal.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef} from "react";
+import { useNavigate } from 'react-router-dom';
 import Modal from "../modal/modal";
 import emailjs from "emailjs-com";
 // import "../../stylesheet/styles.css";
