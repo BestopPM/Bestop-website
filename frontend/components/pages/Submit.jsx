d1e0d4568d6d234fcfc1b9169e046f0d075ea7cf
pearlhchang <p.chang1802@gmail.com>
2023-12-02 00:39:16 -0800

    update submit message

--- frontend/components/pages/Submit.jsx
+++ frontend/components/pages/Submit.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Submit = () => {
     return (
         <main>
-            <header className='header'>Request Submission</header>
-            <h3 className='submit_text'>Your request has been recieved. A property manager should reach out to you within 24 business hours.</h3>
+            <header className='header'>Message Submitted</header>
+            <h3 className='submit_text'> Your message has been received. A property manager should reach out to you within 24 business hours.</h3>
         </main>
     )
 }
 
-export default Submit
+export default Submit;
