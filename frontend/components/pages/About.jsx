719127cb88c82563db928e8629a6c6e3f02b60bc
pearlhchang <p.chang1802@gmail.com>
2023-12-02 20:27:50 -0800

    add styling for about page

--- frontend/components/pages/About.jsx
+++ frontend/components/pages/About.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import aboutPhoto from "../assets/about-photo.webp";
+import "./About.scss"
 
 const About = () => {
   return(
     <>
       <header className="header">About Us</header>
       
-      <main>
-        <h3 className="title">Our Mission</h3>
-        <p>
-        BESTOP PROPERTY MANAGEMENT provides leasing and property management services to investment property owners.  We specialize in residential properties and offer a high-touch, high-quality approach to all our services.
-        </p>
+      <main className="aboutContent__container">
+        <section className="aboutContent__container--top">
+          <h3 className="title">Our Mission</h3>
+          <p>
+          BESTOP PROPERTY MANAGEMENT provides leasing and property management services to investment property owners.  We specialize in residential properties and offer a high-touch, high-quality approach to all our services.
+          </p>
+        </section>
+        <section className="aboutContent__container--bottom">
+          <img src={aboutPhoto} alt="housesPhoto" className="about__img"/>
+        </section>
       </main>
     </>
   )
