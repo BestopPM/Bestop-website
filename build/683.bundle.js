382dfccbeaa98974d3733efd34d7840950835f1f
HShaw215 <hu.shaw215@gmail.com>
2023-12-10 16:09:55 -0500

    fixed bundle pathing

---
+++ build/683.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbestop_landing=self.webpackChunkbestop_landing||[]).push([[683],{683:(e,n,s)=>{s.r(n),s.d(n,{default:()=>c});var t=s(294);const a=s.p+"598d13328654163e0a3d7046b996fe00.webp",c=function(){return t.createElement("img",{src:a,width:"180",height:"82",alt:"BesTop Logo",className:"logo__img",decoding:"async"})}}}]);
