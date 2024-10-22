7fdb2e4d510687043e52d4326084b93ee2278170
pearlhchang <p.chang1802@gmail.com>
2024-10-21 17:57:51 -0700

    update image slider

--- frontend/components/lazyComponents/lazyRental.jsx
+++ frontend/components/lazyComponents/lazyRental.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from "lucide-react";
 import img1 from "../assets/prop1.png";
 import img2 from "../assets/prop2.JPEG";
 import "./lazyRental.scss";
@@ -25,14 +25,27 @@ const LazyRental = () => {
 
   return (
     <div style={{ width: "100%", height: "100%", position: "relative" }}>
-      <img
-        src={IMAGES[imageIndex]}
-        width="450"
-        height="300"
-        alt="propertyImage"
-        className="img-slider-img"
-        decoding="async"
-      />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          overflow: "hidden",
+        }}
+      >
+        {IMAGES.map((url) => (
+          <img
+            key={url}
+            src={url}
+            width="450"
+            height="300"
+            alt="propertyImage"
+            className="img-slider-img"
+            decoding="async"
+            style={{ translate: `${-100 * imageIndex}%` }}
+          />
+        ))}
+      </div>
       <button
         onClick={showPrevImage}
         className="img-slider-btn"
@@ -47,6 +60,27 @@ const LazyRental = () => {
       >
         <ArrowBigRight />
       </button>
+      <div
+        style={{
+          position: "absolute",
+          bottom: ".5rem",
+          left: "50%",
+          translate: "-50%",
+          bottom: "10%",
+          display: "flex",
+          gap: ".25rem",
+        }}
+      >
+        {IMAGES.map((_, index) => (
+          <button
+            key={index}
+            className="img-slider-dot-btn"
+            onClick={() => setImageIndex(index)}
+          >
+            {index === imageIndex ? <CircleDot /> : <Circle />}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
